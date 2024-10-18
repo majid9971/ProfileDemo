ab6a95d5203a5321213fb39d6f41a43f9603d74a
Majid Mohammednisar Siddiqui <majids@cybage.com>
2024-10-18 15:27:13 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String mode = System.getProperty("app.mode");
+        System.out.println("LINUX-AGENT");
+        System.out.println("Application is running in mode: " + mode);
+    }
+}
